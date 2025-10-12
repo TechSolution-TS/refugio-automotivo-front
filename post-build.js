3e20d79749baf127dd95f7b364d453bdc5643d98
Lucas Belém <lucas.belem@usto.re>
2025-10-12 11:28:17 -0300

    justando variables

--- post-build.js
+++ post-build.js
@@ -7,7 +7,7 @@ function replaceInFile(filePath) {
   let content = fs.readFileSync(filePath, 'utf8');
   
   content = content.replace(/NG_APP_API_URL_PLACEHOLDER/g, process.env.NG_APP_API_URL || 'http://localhost:8080');
-  content = content.replace(/NG_APP_API_KEY_PLACEHOLDER/g, process.env.NG_APP_API_KEY || 'JURIDICO_TESTE');
+  content = content.replace(/NG_APP_API_KEY_PLACEHOLDER/g, process.env.NG_APP_API_KEY || 'JURIDICO_TESTE'); 
   
   fs.writeFileSync(filePath, content);
 }
